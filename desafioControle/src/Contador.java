92a823b10656ae082c8eeb31bde1cc6176524989
gabrielkimura <gabrielkimura7@gmal.com>
2023-12-20 18:36:29 -0300

    desafio iphone

---
+++ desafioControle/src/Contador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        try (Scanner tScanner = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro: ");
+            int parametroUm = tScanner.nextInt();
+            System.out.println("Digite o segundo parâmetro: ");
+            int parametroDois = tScanner.nextInt();
+
+            try{
+                contar(parametroUm, parametroDois);
+            }catch(ParametrosInvalidosException e){
+                throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            }
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois){
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for(int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo numero " + i);
+        }
+    }
+}
